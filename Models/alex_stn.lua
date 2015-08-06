ab16f413bc99c308c90c9b47d99be8af6cf94713
feng@necserver <zhfe99@gmail.com>
2015-08-06 13:59:25 -0700

    store

---
+++ Models/alex_stn.lua
@@ -0,0 +1,92 @@
+#!/usr/bin/env th
+-- AlexNet Model.
+--
+-- History
+--   create  -  Feng Zhou (zhfe99@gmail.com), 08-04-2015
+--   modify  -  Feng Zhou (zhfe99@gmail.com), 08-06-2015
+
+----------------------------------------------------------------------
+-- Create the alexnet model.
+--
+-- Input
+--   nC     -  #classes
+--   nGpu   -  #gpus
+--   isBn   -  flag of using BN, true | false
+--   iniAlg -  init method
+--
+-- Output
+--   model  -  model
+function newModel(nC, nGpu, isBn, iniAlg)
+  require 'cudnn'
+  require 'cunn'
+  require 'stn'
+  local SpatialConvolution = cudnn.SpatialConvolution
+  local SpatialMaxPooling = cudnn.SpatialMaxPooling
+
+  local features = nn.Sequential()
+  features:add(SpatialConvolution(3,96,11,11,4,4,2,2))       -- 224 -> 55
+  if isBn then
+    features:add(nn.SpatialBatchNormalization(96, 1e-3))
+  end
+  features:add(cudnn.ReLU(true))
+  features:add(SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
+  features:add(SpatialConvolution(96,256,5,5,1,1,2,2))       --  27 -> 27
+  if isBn then
+    features:add(nn.SpatialBatchNormalization(256, 1e-3))
+  end
+  features:add(cudnn.ReLU(true))
+  features:add(SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
+  features:add(SpatialConvolution(256,384,3,3,1,1,1,1))      --  13 ->  13
+  if isBn then
+    features:add(nn.SpatialBatchNormalization(384, 1e-3))
+  end
+  features:add(cudnn.ReLU(true))
+  features:add(SpatialConvolution(384,256,3,3,1,1,1,1))      --  13 ->  13
+  if isBn then
+    features:add(nn.SpatialBatchNormalization(256, 1e-3))
+  end
+  features:add(cudnn.ReLU(true))
+  features:add(SpatialConvolution(256,256,3,3,1,1,1,1))      --  13 ->  13
+  if isBn then
+    features:add(nn.SpatialBatchNormalization(256, 1e-3))
+  end
+  features:add(cudnn.ReLU(true))
+  features:add(SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
+
+  local classifier = nn.Sequential()
+  classifier:add(nn.View(256*6*6))
+  classifier:add(nn.Dropout(0.5))
+  classifier:add(nn.Linear(256*6*6, 4096))
+  if isBn then
+    classifier:add(nn.BatchNormalization(4096, 1e-3))
+  end
+  classifier:add(nn.Threshold(0, 1e-6))
+  classifier:add(nn.Dropout(0.5))
+  classifier:add(nn.Linear(4096, 4096))
+  if isBn then
+    classifier:add(nn.BatchNormalization(4096, 1e-3))
+  end
+  classifier:add(nn.Threshold(0, 1e-6))
+  classifier:add(nn.Linear(4096, nC))
+  classifier:add(nn.LogSoftMax())
+
+  -- function fillBias(m)
+  --   for i=1, #m.modules do
+  --     if m:get(i).bias then
+  --       m:get(i).bias:fill(0.1)
+  --     end
+  --   end
+  -- end
+  -- fillBias(features)
+  -- fillBias(classifier)
+
+  local model = nn.Sequential()
+  model:add(features):add(classifier)
+
+  -- init
+  w_init = require('weight_init')
+  model.modules[1] = w_init(model.modules[1], iniAlg)
+  model.modules[2] = w_init(model.modules[2], iniAlg)
+
+  return model
+end
--- Models/car_v1c_alexbn_stn.lua
+++ Models/car_v1c_alexbn_stn.lua
@@ -1,4 +1,4 @@
-paths.dofile('alex.lua')
+paths.dofile('alex_stn.lua')
 
 local nC = 196
 
@@ -11,9 +11,12 @@ local nEpo = 120
 local nEpoSv = 40
 
 local lrs = {
-  {  1,   40, 1e-2, 5e-4},
-  { 41,   80, 1e-3, 5e-4},
-  { 81, nEpo, 1e-4, 5e-4}
+  {  1,   20, 1e-2, 5e-4},
+  { 21,   40, 5e-3, 5e-4},
+  { 41,   60, 1e-3, 5e-4},
+  { 61,   80, 5e-4, 5e-4},
+  { 81,  100, 1e-4, 5e-4},
+  {101, nEpo, 5e-5, 5e-4},
 }
 
 local batchSiz = 128
