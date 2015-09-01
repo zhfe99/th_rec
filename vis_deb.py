264a0f8afb2d86a188713c69eb1cad491d4228da
feng@feng-mbp <feng@feng-mac>
2015-09-01 16:44:59 -0700

    store

--- vis_deb.py
+++ vis_deb.py
@@ -116,49 +116,48 @@ def shEpoGrad(dbe, ver, con, nStn, epo):
     tmpFold = os.path.join(os.environ['HOME'],
                            'save/{}/torch/tmp/{}_{}_{}'.format(dbe, dbe, ver, con))
     pdfFold = os.path.join(os.environ['HOME'],
-                           'save/{}/torch/deb_stn/{}_{}_{'.format(dbe, dbe, ver, con))
+                           'save/{}/torch/deb_stn/{}_{}_{}'.format(dbe, dbe, ver, con))
     lib.mkDir(pdfFold)
 
     # path
-    h5Path = '{}/train_{}_{}.h5'.format(tmpFold, epo, 1)
+    h5Path = '{}/train_{}_{}_grad.h5'.format(tmpFold, epo, 1)
     pdfPath = '{}/train_{}_{}_grad.pdf'.format(pdfFold, epo, 1)
 
     # read from hdf
     ha = lib.hdfRIn(h5Path)
-    inputs, grids = [], []
+    imgOuts, imgGrads, gridOuts, gridGrads = [], [], [], []
     for iStn in range(nStn):
-        gridi = lib.hdfR(ha, 'grid{}'.format(iStn + 1))
-        inputi = lib.hdfR(ha, 'input{}'.format(iStn + 1))
-        grids.append(gridi)
-        inputs.append(inputi)
-    input0 = lib.hdfR(ha, 'input0')
+        imgOuts.append(lib.hdfR(ha, 'imgOut{}'.format(iStn + 1)))
+        imgGrads.append(lib.hdfR(ha, 'imgGrad{}'.format(iStn + 1)))
+        gridOuts.append(lib.hdfR(ha, 'gridOut{}'.format(iStn + 1)))
+        gridGrads.append(lib.hdfR(ha, 'gridGrad{}'.format(iStn + 1)))
+    imgOut0 = lib.hdfR(ha, 'imgOut0')
     # bias = lib.hdfR(ha, 'bias')
     # weight = lib.hdfR(ha, 'weight')
     lib.hdfROut(ha)
 
     # dimension
-    n, h, w, _ = grids[0].shape
-    nTop = min(input0.shape[0], 7)
+    n, d, h, w = imgOut0.shape
+    nTop = min(n, 7)
 
     # show
-    cols = nTop + 1
-    Ax = lib.iniAx(1, nStn * 2, cols, [3 * nStn * 2, 3 * cols], flat=False)
-
-    lib.setAx(Ax[0, nTop])
-    lib.plt.axis('off')
+    cols = nTop
+    rowStn = 4
+    Ax = lib.iniAx(1, nStn * rowStn, cols, [3 * nStn * rowStn, 3 * cols], flat=False)
 
     # each transformer
     for iStn in range(nStn):
-        input = inputs[iStn]
-        grid = grids[iStn]
+        imgOut = imgOuts[iStn]
+        imgGrad = imgGrads[iStn]
+        gridOut = gridOuts[iStn]
+        gridGrad = gridGrads[iStn]
 
         # each example
         for iTop in range(nTop):
             col = iTop
 
             # original input
-            input0New = input0[iTop].transpose((1, 2, 0))
-            lib.shImg(input0New, ax=Ax[iStn * 2, col])
+            lib.shImg(imgOut0[iTop].transpose((1, 2, 0)), ax=Ax[iStn * rowStn, col])
 
             idxYs = [0, 0, h - 1, h - 1, 0]
             idxXs = [0, w - 1, w - 1, 0, 0]
@@ -167,23 +166,37 @@ def shEpoGrad(dbe, ver, con, nStn, epo):
                 idxY = idxYs[i]
                 idxX = idxXs[i]
 
-                ys[i] = (grid[iTop, idxY, idxX, 0] + 1) / 2 * h
-                xs[i] = (grid[iTop, idxY, idxX, 1] + 1) / 2 * w
+                ys[i] = (gridOut[iTop, idxY, idxX, 0] + 1) / 2 * h
+                xs[i] = (gridOut[iTop, idxY, idxX, 1] + 1) / 2 * w
             lib.plt.plot(xs, ys, 'r-')
-            lib.plt.axis('image')
+            # lib.plt.axis('image')
 
-            # input
-            inputNew = input[iTop].transpose((1, 2, 0))
-            lib.shImg(inputNew, ax=Ax[iStn * 2 + 1, col])
+            # crop input
+            lib.shImg(imgOut[iTop].transpose((1, 2, 0)), ax=Ax[iStn * rowStn + 1, col])
+
+            # image gradient
+            imgGradMa = imgGrad[iTop].max()
+            imgGradMi = imgGrad[iTop].min()
+            lib.shImg((imgGrad[iTop] - imgGradMi) / (imgGradMa - imgGradMi), ax=Ax[iStn * rowStn + 2, col])
+
+            # grid gradient
+            lib.setAx(Ax[iStn * rowStn + 3, col])
+            GX = gridGrad[iTop][:, :, 0]
+            GY = gridGrad[iTop][:, :, 1]
+            Q = lib.plt.quiver(GX, GY)
+            qk = lib.plt.quiverkey(Q, 0.5, 0.92, 2, '', labelpos='W')
+            # lib.plt.axis('image')
+            lib.plt.gca().invert_yaxis()
 
         # mean
-        inMe0 = input0.mean(0)
-        inMe = input.mean(0)
-        lib.shImg(inMe0.transpose((1, 2, 0)), ax=Ax[iStn * 2, nTop])
-        lib.shImg(inMe.transpose((1, 2, 0)), ax=Ax[iStn * 2 + 1, nTop])
+        # inMe0 = input0.mean(0)
+        # inMe = input.mean(0)
+        # lib.shImg(inMe0.transpose((1, 2, 0)), ax=Ax[iStn * 2, nTop])
+        # lib.shImg(inMe.transpose((1, 2, 0)), ax=Ax[iStn * 2 + 1, nTop])
 
     # save
     # lib.show()
+    # import pdb; pdb.set_trace()
     lib.shSvPath(pdfPath)
 
 if __name__ == '__main__':
@@ -209,4 +222,5 @@ if __name__ == '__main__':
         epo = epos[iEpo]
 
         shEpoTran(dbe, ver, con, nStn, epo)
+        shEpoGrad(dbe, ver, con, nStn, epo)
     lib.prCOut(nEpo)
